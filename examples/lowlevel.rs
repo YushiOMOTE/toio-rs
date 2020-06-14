abd177275f09b929c49de407c6308f132135ff0f
Yushi OMOTE <yushiomote@gmail.com>
2020-06-14 17:16:58 +0900

    Add low-level API example

---
+++ examples/lowlevel.rs
@@ -0,0 +1,38 @@
+use toio::{
+    ble::{self, PeripheralOps, PeripheralOpsExt},
+    proto::*,
+};
+
+use std::time::Duration;
+use tokio::time::delay_for;
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    let mut searcher = ble::searcher();
+    let mut peripherals = searcher.search(&UUID_SERVICE).await.unwrap();
+    let mut peripheral = peripherals.pop().unwrap();
+
+    peripheral.connect().await.unwrap();
+
+    delay_for(Duration::from_secs(2)).await;
+
+    peripheral
+        .write_msg(
+            &UUID_MOTOR,
+            Motor::Simple(MotorSimple::new(
+                MotorId::Left,
+                MotorDir::Forward,
+                30,
+                MotorId::Right,
+                MotorDir::Forward,
+                30,
+            )),
+            false,
+        )
+        .await
+        .unwrap();
+
+    delay_for(Duration::from_secs(2)).await;
+}
