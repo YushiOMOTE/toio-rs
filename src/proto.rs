5c03c781c9761dc55aa553c79f5b9d946fa44217
Yushi OMOTE <yushiomote@gmail.com>
2020-06-14 16:58:34 +0900

    Add sound note enum

--- src/proto.rs
+++ src/proto.rs
@@ -33,6 +33,270 @@ pub const UUID_BATTERY: Uuid = uuid!("10b20108 5b3b 4571 9508 cf3efcd7bbae");
 /// The UUID of the configuration characteristic.
 pub const UUID_CONFIG: Uuid = uuid!("10b201ff 5b3b 4571 9508 cf3efcd7bbae");
 
+/// Sound note.
+#[derive(Serialize_repr, Deserialize_repr, Debug, Clone, PartialEq, Eq)]
+#[repr(u8)]
+pub enum Note {
+    /// C 0
+    C0 = 0,
+    /// C# 0
+    CS0,
+    /// D 0
+    D0,
+    /// D# 0
+    DS0,
+    /// E 0
+    E0,
+    /// F 0
+    F0,
+    /// F# 0
+    FS0,
+    /// G 0
+    G0,
+    /// G# 0
+    GS0,
+    /// A 0
+    A0,
+    /// A# 0
+    AS0,
+    /// B 0
+    B0,
+    /// C 1
+    C1,
+    /// C# 1
+    CS1,
+    /// D 1
+    D1,
+    /// D# 1
+    DS1,
+    /// E 1
+    E1,
+    /// F 1
+    F1,
+    /// F# 1
+    FS1,
+    /// G 1
+    G1,
+    /// G# 1
+    GS1,
+    /// A 1
+    A1,
+    /// A# 1
+    AS1,
+    /// B 1
+    B1,
+    /// C 2
+    C2,
+    /// C# 2
+    CS2,
+    /// D 2
+    D2,
+    /// D# 2
+    DS2,
+    /// E 2
+    E2,
+    /// F 2
+    F2,
+    /// F# 2
+    FS2,
+    /// G 2
+    G2,
+    /// G# 2
+    GS2,
+    /// A 2
+    A2,
+    /// A# 2
+    AS2,
+    /// B 2
+    B2,
+    /// C 3
+    C3,
+    /// C# 3
+    CS3,
+    /// D 3
+    D3,
+    /// D# 3
+    DS3,
+    /// E 3
+    E3,
+    /// F 3
+    F3,
+    /// F# 3
+    FS3,
+    /// G 3
+    G3,
+    /// G# 3
+    GS3,
+    /// A 3
+    A3,
+    /// A# 3
+    AS3,
+    /// B 3
+    B3,
+    /// C 4
+    C4,
+    /// C# 4
+    CS4,
+    /// D 4
+    D4,
+    /// D# 4
+    DS4,
+    /// E 4
+    E4,
+    /// F 4
+    F4,
+    /// F# 4
+    FS4,
+    /// G 4
+    G4,
+    /// G# 4
+    GS4,
+    /// A 4
+    A4,
+    /// A# 4
+    AS4,
+    /// B 4
+    B4,
+    /// C 5
+    C5,
+    /// C# 5
+    CS5,
+    /// D 5
+    D5,
+    /// D# 5
+    DS5,
+    /// E 5
+    E5,
+    /// F 5
+    F5,
+    /// F# 5
+    FS5,
+    /// G 5
+    G5,
+    /// G# 5
+    GS5,
+    /// A 5
+    A5,
+    /// A# 5
+    AS5,
+    /// B 5
+    B5,
+    /// C 6
+    C6,
+    /// C# 6
+    CS6,
+    /// D 6
+    D6,
+    /// D# 6
+    DS6,
+    /// E 6
+    E6,
+    /// F 6
+    F6,
+    /// F# 6
+    FS6,
+    /// G 6
+    G6,
+    /// G# 6
+    GS6,
+    /// A 6
+    A6,
+    /// A# 6
+    AS6,
+    /// B 6
+    B6,
+    /// C 7
+    C7,
+    /// C# 7
+    CS7,
+    /// D 7
+    D7,
+    /// D# 7
+    DS7,
+    /// E 7
+    E7,
+    /// F 7
+    F7,
+    /// F# 7
+    FS7,
+    /// G 7
+    G7,
+    /// G# 7
+    GS7,
+    /// A 7
+    A7,
+    /// A# 7
+    AS7,
+    /// B 7
+    B7,
+    /// C 8
+    C8,
+    /// C# 8
+    CS8,
+    /// D 8
+    D8,
+    /// D# 8
+    DS8,
+    /// E 8
+    E8,
+    /// F 8
+    F8,
+    /// F# 8
+    FS8,
+    /// G 8
+    G8,
+    /// G# 8
+    GS8,
+    /// A 8
+    A8,
+    /// A# 8
+    AS8,
+    /// B 8
+    B8,
+    /// C 9
+    C9,
+    /// C# 9
+    CS9,
+    /// D 9
+    D9,
+    /// D# 9
+    DS9,
+    /// E 9
+    E9,
+    /// F 9
+    F9,
+    /// F# 9
+    FS9,
+    /// G 9
+    G9,
+    /// G# 9
+    GS9,
+    /// A 9
+    A9,
+    /// A# 9
+    AS9,
+    /// B 9
+    B9,
+    /// C 10
+    C10,
+    /// C# 10
+    CS10,
+    /// D 10
+    D10,
+    /// D# 10
+    DS10,
+    /// E 10
+    E10,
+    /// F 10
+    F10,
+    /// F# 10
+    FS10,
+    /// G 10
+    G10,
+    /// No sound.
+    NoSound,
+}
+
 macro_rules! msg {
     ($(#[$attr:meta])?pub enum $name:tt {
         $(
@@ -523,7 +787,7 @@ pub struct SoundOp {
     /// The duration to play the sound.
     pub duration: u8,
     /// The sound note.
-    pub note: u8,
+    pub note: Note,
     /// The sound volume.
     pub vol: u8,
 }
