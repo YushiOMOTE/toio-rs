85d7740edac963c4f13c7d018ffd5cf1e3dc54aa
Yushi OMOTE <yushiomote@gmail.com>
2020-06-15 07:48:29 +0900

    Test version and light ctrl messages

--- tests/proto.rs
+++ tests/proto.rs
@@ -32,3 +32,35 @@ fn test_button() {
     let p: Button = p.try_into().unwrap();
     assert_eq!(p, Button::Func(ButtonState::Pressed));
 }
+
+#[test]
+fn test_version() {
+    let p: Vec<u8> = Config::VersionRes(ConfigVersionRes::new("testXY".into()))
+        .try_into()
+        .unwrap();
+    assert_eq!(p, vec![0x81, 0x00, 0x74, 0x65, 0x73, 0x74, 0x58, 0x59]);
+    let p: Config = p.try_into().unwrap();
+    assert_eq!(
+        p,
+        Config::VersionRes(ConfigVersionRes::new("testXY".into()))
+    );
+}
+
+#[test]
+fn test_light() {
+    let l = Light::Ctrl(LightCtrl::new(
+        0,
+        2,
+        vec![LightOn::new(1, 2, 3, 4), LightOn::new(5, 6, 7, 8)],
+    ));
+    let p: Vec<u8> = l.clone().try_into().unwrap();
+    assert_eq!(
+        p,
+        vec![
+            0x04, 0x00, 0x02, 0x01, 0x01, 0x01, 0x02, 0x03, 0x04, 0x05, 0x01, 0x01, 0x06, 0x07,
+            0x08
+        ]
+    );
+    let p: Light = p.try_into().unwrap();
+    assert_eq!(p, l);
+}
