931a6bff9342fe1c7945c96999b27f0ec1faba93
Yushi OMOTE <yushiomote@gmail.com>
2020-06-15 11:52:51 +0900

    Add sound example

---
+++ examples/sound.rs
@@ -0,0 +1,37 @@
+use std::time::Duration;
+use toio::{Cube, Note, Sound, SoundOp, SoundOps, SoundPresetId};
+use tokio::time::delay_for;
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    // Search for the nearest cube
+    let mut cube = Cube::search().nearest().await.unwrap();
+
+    // Connect
+    cube.connect().await.unwrap();
+
+    delay_for(Duration::from_secs(1)).await;
+
+    cube.play(Sound::Preset(SoundPresetId::Enter))
+        .await
+        .unwrap();
+
+    delay_for(Duration::from_secs(2)).await;
+
+    cube.play(Sound::Ops(SoundOps::new(
+        vec![
+            SoundOp::new(Note::C5, Duration::from_millis(500)),
+            SoundOp::new(Note::A6, Duration::from_millis(500)),
+        ],
+        3,
+    )))
+    .await
+    .unwrap();
+
+    delay_for(Duration::from_secs(4)).await;
+
+    // Stop
+    cube.stop().await.unwrap();
+}
